b23a0e9b0f95ed16d520c5685a2fa93a94be8a10
Vitor Mendes <vitor.mendes@gmail.com>
2022-06-18 22:10:23 -0300

    app finished

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,9 +5,9 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SignInButton() {
   const { data: session } = useSession();
-  
+
   return session ? (
-    <button type="button" className={styles.signInButton} onClick={()=> signOut()}>
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04D361" />
       {session.user.name}
       <FiX color="737380" className={styles.closeIcon} />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,6 +18,31 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    async session({session}) {
+      try {
+        const userActiveSubsription = await Fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select("ref", q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email))))
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubsription,
+        };
+      } catch (error) {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn({ user, account, profile }) {
       const { email } = user;
 
